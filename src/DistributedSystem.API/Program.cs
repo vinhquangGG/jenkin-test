18302e22d609c0363a029585ca4a9bbde0bcdf6b
QuangVinh.GG <vinhquangGG@gmail.com>
2026-06-28 11:00:42 +0700

    a

--- src/DistributedSystem.API/Program.cs
+++ src/DistributedSystem.API/Program.cs
@@ -1,19 +1,16 @@
-using Carter;
+﻿using Carter;
 using DistributedSystem.API.DependencyInjection.Extensions;
 using DistributedSystem.API.Middleware;
 using DistributedSystem.Application.DependencyInjection.Extensions;
-using DistributedSystem.Persistence.DependencyInjection.Extensions;
-using DistributedSystem.Persistence.DependencyInjection.Options;
+// using DistributedSystem.Persistence.DependencyInjection.Extensions;
 using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using DistributedSystem.Infrastructure.DependencyInjection.Extensions;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add configuration
-
-Log.Logger = new LoggerConfiguration().ReadFrom
-    .Configuration(builder.Configuration)
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
     .CreateLogger();
 
 builder.Logging
@@ -23,36 +20,32 @@ builder.Logging
 builder.Host.UseSerilog();
 
 builder.Services.AddInfrastructureServices();
-builder.Services.AddRedisService(builder.Configuration);
+
+// ======= Tạm thời comment =======
+// builder.Services.AddRedisService(builder.Configuration);
+
+// builder.Services.ConfigureSqlServerRetryOptions(
+//     builder.Configuration.GetSection(nameof(SqlServerRetryOptions)));
+
+// builder.Services.AddInterceptorDbContext();
+// builder.Services.AddSqlConfiguration();
+// builder.Services.AddRepositoryBaseConfiguration();
+// ================================
 
 builder.Services.AddJwtAuthentication(builder.Configuration);
-
-//builder
-//    .Services
-//    .AddControllers()
-//    .AddApplicationPart(DistributedSystem.Presentation.AssemblyReference.Assembly);
 
 builder.Services.AddConfigureMediatR();
 builder.Services.AddConfigureAutoMapper();
 
-// Add Middleware => Remember using middleware
 builder.Services.AddTransient<ExceptionHandlingMiddleware>();
 
-// Configure Options and SQL => Remember mapcarter
-builder.Services.AddInterceptorDbContext();
-builder.Services.ConfigureSqlServerRetryOptions(builder.Configuration.GetSection(nameof(SqlServerRetryOptions)));
-builder.Services.AddSqlConfiguration();
-builder.Services.AddRepositoryBaseConfiguration();
-
-// Add Carter module
 builder.Services.AddCarter();
 
-// Add Swagger
 builder.Services
-        .AddSwaggerGenNewtonsoftSupport()
-        .AddFluentValidationRulesToSwagger()
-        .AddEndpointsApiExplorer()
-        .AddSwagger();
+    .AddSwaggerGenNewtonsoftSupport()
+    .AddFluentValidationRulesToSwagger()
+    .AddEndpointsApiExplorer()
+    .AddSwagger();
 
 builder.Services
     .AddApiVersioning(options => options.ReportApiVersions = true)
@@ -64,37 +57,14 @@ builder.Services
 
 var app = builder.Build();
 
-// Using middleware
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
-//app.UseHttpsRedirection();
-
-app.UseAuthentication(); // This need to be added before UseAuthorization
+app.UseAuthentication();
 app.UseAuthorization();
 
-//app.MapControllers();
-
-// Add API Endpoint with carter module
 app.MapCarter();
 
-// Configure the HTTP request pipeline. 
-if (builder.Environment.IsDevelopment() || builder.Environment.IsStaging())
-    app.ConfigureSwagger(); // => After MapCarter => Show Version
+// Để deploy IIS cũng có Swagger, tạm bỏ điều kiện môi trường
+app.ConfigureSwagger();
 
-try
-{
-await app.RunAsync();
-Log.Information("Stopped cleanly");
-}
-catch (Exception ex)
-{
-Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
-await app.StopAsync();
-}
-finally
-{
-Log.CloseAndFlush();
-await app.DisposeAsync();
-}
-
-public partial class Program { }
+app.Run();
