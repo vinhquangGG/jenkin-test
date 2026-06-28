45920e202eeffd18063e246c85692f89555db3f6
QuangVinh.GG <vinhquangGG@gmail.com>
2026-06-28 11:36:53 +0700

    xxa

--- src/DistributedSystem.API/Program.cs
+++ src/DistributedSystem.API/Program.cs
@@ -1,83 +1,13 @@
-﻿using Carter;
-using DistributedSystem.API.DependencyInjection.Extensions;
-using DistributedSystem.API.Middleware;
-using DistributedSystem.Application.DependencyInjection.Extensions;
-using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
-using Serilog;
+﻿var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Logging
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration)
-    .CreateLogger();
-
-builder.Logging.ClearProviders();
-builder.Logging.AddSerilog();
-builder.Host.UseSerilog();
-
-// ===== Infrastructure =====
-
-// builder.Services.AddInfrastructureServices();
-// builder.Services.AddRedisService(builder.Configuration);
-
-// Nếu không dùng JWT thì comment luôn
-// builder.Services.AddJwtAuthentication(builder.Configuration);
-
-// ===== Application =====
-
-// Nếu Handler của bạn phụ thuộc Repository thì comment luôn
-// builder.Services.AddConfigureMediatR();
-
-builder.Services.AddConfigureAutoMapper();
-
-// ===== Middleware =====
-
-builder.Services.AddTransient<ExceptionHandlingMiddleware>();
-
-// ===== Database =====
-
-// builder.Services.AddInterceptorDbContext();
-
-// builder.Services.ConfigureSqlServerRetryOptions(
-//     builder.Configuration.GetSection(nameof(SqlServerRetryOptions)));
-
-// builder.Services.AddSqlConfiguration();
-
-// builder.Services.AddRepositoryBaseConfiguration();
-
-// ===== Carter =====
-
-builder.Services.AddCarter();
-
-// ===== Swagger =====
-
-builder.Services
-    .AddEndpointsApiExplorer()
-    .AddSwaggerGenNewtonsoftSupport()
-    .AddFluentValidationRulesToSwagger()
-    .AddSwagger();
-
-builder.Services
-    .AddApiVersioning(options =>
-    {
-        options.ReportApiVersions = true;
-    })
-    .AddApiExplorer(options =>
-    {
-        options.GroupNameFormat = "'v'VVV";
-        options.SubstituteApiVersionInUrl = true;
-    });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-// app.UseAuthentication();
-// app.UseAuthorization();
+app.MapGet("/", () => "Hello Jenkins");
 
-app.MapCarter();
-
-app.ConfigureSwagger();
-
-await app.RunAsync();
+app.Run();
