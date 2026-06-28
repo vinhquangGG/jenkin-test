bea457e543e2f665375cb707549d03aca45e0111
QuangVinh.GG <vinhquangGG@gmail.com>
2026-06-28 11:21:42 +0700

    a

--- src/DistributedSystem.API/Program.cs
+++ src/DistributedSystem.API/Program.cs
@@ -1,70 +1,13 @@
-﻿using Carter;
-using DistributedSystem.API.DependencyInjection.Extensions;
-using DistributedSystem.API.Middleware;
-using DistributedSystem.Application.DependencyInjection.Extensions;
-// using DistributedSystem.Persistence.DependencyInjection.Extensions;
-using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
-using DistributedSystem.Infrastructure.DependencyInjection.Extensions;
-using Serilog;
+﻿var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
-
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration)
-    .CreateLogger();
-
-builder.Logging
-    .ClearProviders()
-    .AddSerilog();
-
-builder.Host.UseSerilog();
-
-builder.Services.AddInfrastructureServices();
-
-// ======= Tạm thời comment =======
-// builder.Services.AddRedisService(builder.Configuration);
-
-// builder.Services.ConfigureSqlServerRetryOptions(
-//     builder.Configuration.GetSection(nameof(SqlServerRetryOptions)));
-
-// builder.Services.AddInterceptorDbContext();
-// builder.Services.AddSqlConfiguration();
-// builder.Services.AddRepositoryBaseConfiguration();
-// ================================
-
-builder.Services.AddJwtAuthentication(builder.Configuration);
-
-builder.Services.AddConfigureMediatR();
-builder.Services.AddConfigureAutoMapper();
-
-builder.Services.AddTransient<ExceptionHandlingMiddleware>();
-
-builder.Services.AddCarter();
-
-builder.Services
-    .AddSwaggerGenNewtonsoftSupport()
-    .AddFluentValidationRulesToSwagger()
-    .AddEndpointsApiExplorer()
-    .AddSwagger();
-
-builder.Services
-    .AddApiVersioning(options => options.ReportApiVersions = true)
-    .AddApiExplorer(options =>
-    {
-        options.GroupNameFormat = "'v'VVV";
-        options.SubstituteApiVersionInUrl = true;
-    });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapCarter();
-
-// Để deploy IIS cũng có Swagger, tạm bỏ điều kiện môi trường
-app.ConfigureSwagger();
+app.MapGet("/", () => "Hello Jenkins");
 
 app.Run();
